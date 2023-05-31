cb8de5fd2edee3cbcda61fafac87d7b0fb3a0df2
NguyenGiaHy123 <89922176+NguyenGiaHy123@users.noreply.github.com>
2023-05-31 10:37:01 +0700

    profileapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{useState,useEffect}from 'react';
+
 import './App.css';
+import {Slidebar} from './Component/SlidBar/Slidebar';
+import {Profile} from './Component/Profile/Profile';
+
+
 
 function App() {
+  const [theme, setTheme] = useState('');
+  const localStorageColor=JSON.parse(localStorage.getItem('theme')||'');
+
+  useEffect(() => {
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      setTheme('dark');
+    }
+    else {
+      setTheme('light');
+    }
+  }, [])
+
+  useEffect(() => {
+    if (localStorageColor=== "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+    if(theme==='dark'){
+      localStorage.setItem('theme',JSON.stringify('light'))
+    }
+    else{
+      localStorage.setItem('theme',JSON.stringify('dark'))
+    }
+  };
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+ 
+    <main className={`min-h-screen ${localStorageColor=='dark'?'dark:bg-gray-900':'bg-gradient-to-r from-green-400 to-blue-400 '} dark:text-white` }>
+        <div className=' grid grid-cols-12 gap-6 py-14  lg:px-36 md:px-20 px-10'>
+        <div className={`col-span-12 lg:col-span-3 rounded bg-white dark:bg-gray-900`} ><Slidebar  theme={theme} setTheme={handleThemeSwitch}/></div>
+      <div className={`col-span-12 lg:col-span-9 bg-white rounded dark:bg-gray-900 dark:shadow-custom-light`}><Profile  /></div> 
+        </div>
+    </main>
+
+ 
+
+
+  
   );
 }
 
